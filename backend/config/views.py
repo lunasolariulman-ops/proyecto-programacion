9653a05e074fc6768602470e7de51cca6a94933c
Joaquin <ulman.joaquinp@gmail.com>
2026-04-19 20:26:18 -0300

    first-commit

---
+++ backend/config/views.py
@@ -0,0 +1,8 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(['GET'])
+def hola(request):
+    return Response({
+    'mensaje': 'Hola React!'
+    })
