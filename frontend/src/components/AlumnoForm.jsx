e8498d343701ee1fae1d8b55042ae7977cd05f67
Joaquin <ulman.joaquinp@gmail.com>
2026-06-04 20:17:58 -0300

    Cambio AlumnoForm.jsx

--- frontend/src/components/AlumnoForm.jsx
+++ frontend/src/components/AlumnoForm.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import {
-  Card, CardContent, CardHeader, Grid, TextField, MenuItem,
+  Card, CardContent, CardHeader, TextField, MenuItem,
   Button, Stack, Divider, Typography, Box, FormControl,
   FormLabel, RadioGroup, FormControlLabel, Radio, Checkbox, Select, InputLabel,
 } from "@mui/material";
@@ -60,14 +60,10 @@ const AlumnoForm = ({ onGuardarAlumno }) => {
                 <BadgeOutlined color="primary" />
                 <Typography variant="subtitle1" fontWeight={600}>Identificación</Typography>
               </Stack>
-              <Grid container spacing={2}>
-                <Grid item xs={12} sm={6}>
-                  <TextField fullWidth required label="DNI" name="dni" value={formData.dni} onChange={handleChange} />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                  <TextField fullWidth required type="email" label="Email" name="email" value={formData.email} onChange={handleChange} />
-                </Grid>
-              </Grid>
+              <Stack direction={{ xs: "column", sm: "row" }} spacing={2}>
+                <TextField fullWidth required label="DNI" name="dni" value={formData.dni} onChange={handleChange} />
+                <TextField fullWidth required type="email" label="Email" name="email" value={formData.email} onChange={handleChange} />
+              </Stack>
             </Box>
 
             <Box>
@@ -75,38 +71,32 @@ const AlumnoForm = ({ onGuardarAlumno }) => {
                 <MenuBookOutlined color="primary" />
                 <Typography variant="subtitle1" fontWeight={600}>Materia y modalidad</Typography>
               </Stack>
-              <Grid container spacing={2}>
-                <Grid item xs={12} sm={6}>
-                  <FormControl fullWidth required>
-                    <InputLabel>Materia</InputLabel>
-                    <Select name="materia" value={formData.materia} label="Materia" onChange={handleChange}>
-                      <MenuItem value="Programación I">Programación I</MenuItem>
-                      <MenuItem value="Programación II">Programación II</MenuItem>
-                      <MenuItem value="Base de Datos">Base de Datos</MenuItem>
-                      <MenuItem value="Ingeniería de Software">Ingeniería de Software</MenuItem>
-                    </Select>
-                  </FormControl>
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                  <FormControl fullWidth>
-                    <InputLabel>Turno</InputLabel>
-                    <Select name="turno" value={formData.turno} label="Turno" onChange={handleChange}>
-                      <MenuItem value="Mañana">Mañana</MenuItem>
-                      <MenuItem value="Tarde">Tarde</MenuItem>
-                      <MenuItem value="Noche">Noche</MenuItem>
-                    </Select>
-                  </FormControl>
-                </Grid>
-                <Grid item xs={12}>
-                  <FormControl>
-                    <FormLabel>Modalidad</FormLabel>
-                    <RadioGroup row name="modalidad" value={formData.modalidad} onChange={handleChange}>
-                      <FormControlLabel value="Presencial" control={<Radio />} label="Presencial" />
-                      <FormControlLabel value="Virtual" control={<Radio />} label="Virtual" />
-                    </RadioGroup>
-                  </FormControl>
-                </Grid>
-              </Grid>
+              <Stack direction={{ xs: "column", sm: "row" }} spacing={2}>
+                <FormControl sx={{ flex: 1 }} required>
+                  <InputLabel>Materia</InputLabel>
+                  <Select name="materia" value={formData.materia} label="Materia" onChange={handleChange}>
+                    <MenuItem value="Programación I">Programación I</MenuItem>
+                    <MenuItem value="Programación II">Programación II</MenuItem>
+                    <MenuItem value="Base de Datos">Base de Datos</MenuItem>
+                    <MenuItem value="Ingeniería de Software">Ingeniería de Software</MenuItem>
+                  </Select>
+                </FormControl>
+                <FormControl sx={{ flex: 1 }}>
+                  <InputLabel>Turno</InputLabel>
+                  <Select name="turno" value={formData.turno} label="Turno" onChange={handleChange}>
+                    <MenuItem value="Mañana">Mañana</MenuItem>
+                    <MenuItem value="Tarde">Tarde</MenuItem>
+                    <MenuItem value="Noche">Noche</MenuItem>
+                  </Select>
+                </FormControl>
+                <FormControl sx={{ flex: 1 }}>
+                  <FormLabel>Modalidad</FormLabel>
+                  <RadioGroup row name="modalidad" value={formData.modalidad} onChange={handleChange}>
+                    <FormControlLabel value="Presencial" control={<Radio />} label="Presencial" />
+                    <FormControlLabel value="Virtual" control={<Radio />} label="Virtual" />
+                  </RadioGroup>
+                </FormControl>
+              </Stack>
             </Box>
 
             <TextField
