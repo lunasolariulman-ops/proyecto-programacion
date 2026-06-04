0363602bab9275508964cc33d08f2c10b7f198c9
Joaquin <ulman.joaquinp@gmail.com>
2026-06-04 19:42:39 -0300

    Cambio App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,24 @@
 import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import Layout from './components/layout/Layout';
 import Home from './components/Home';
 import AlumnoForm from './components/AlumnoForm';
-import Layout from './components/layout/Layout';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 
-function App() {
+const App = () => {
   const [alumno, setAlumno] = useState(null);
+
   return (
     <Layout>
       <Routes>
         <Route path="/" element={<Home alumno={alumno} />} />
-        <Route
-          path="/registro"
-          element={<AlumnoForm onGuardarAlumno={setAlumno} />} />
+        <Route path="/registro" element={<AlumnoForm onGuardarAlumno={setAlumno} />} />
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard alumno={alumno} />} />
       </Routes>
     </Layout>
   );
-}
+};
 
 export default App;
